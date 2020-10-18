10f5092c07e73655f43a19f71720388ce0cc27f9
Andrey Vlasovskikh <andrey.vlasovskikh@gmail.com>
2020-10-19 02:48:45 +0300

    Refactored draw methods into XduCanvas

--- pyxdu/tk.py
+++ pyxdu/tk.py
@@ -5,45 +5,50 @@ from pyxdu.xdu import Node, Rect, parse_file
 n_cols = 5
 
 
-def draw_node(canvas: Canvas, node: Node, rect: Rect) -> None:
-    draw_rect(canvas, node.name, node.size, rect)
-    node.rect = rect
-    sub_rect = Rect(rect.left + rect.width, rect.top, rect.width, rect.height)
-    draw_children(canvas, node, sub_rect)
+class XduCanvas(Canvas):
+    node: Node
 
+    def __init__(self, node: Node, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.node = node
 
-def draw_children(canvas: Canvas, node: Node, rect: Rect) -> None:
-    # if node.size == 0:
-    #     node.size = sum((c.size for c in node.children))
-    top = rect.top
-    total_size = node.size
+    def draw_node(self, rect: Rect) -> None:
+        self.draw_rect(self.node.name, self.node.size, rect)
+        self.node.rect = rect
+        sub_rect = Rect(rect.left + rect.width, rect.top, rect.width,
+                        rect.height)
+        self.draw_children(self.node, sub_rect)
 
-    for child in node.children:
-        size = child.size / total_size
-        height = int(size * rect.height + 0.5)
-        if height > 1:
-            c_rect = Rect(rect.left, top, rect.width, height)
-            draw_rect(canvas, child.name, child.size, c_rect)
-            child.rect = c_rect
+    def draw_children(self, node: Node, rect: Rect) -> None:
+        # if node.size == 0:
+        #     node.size = sum((c.size for c in node.children))
+        top = rect.top
+        total_size = node.size
 
-            c2_rect = Rect(rect.left + rect.width, top, rect.width, height)
-            draw_children(canvas, child, c2_rect)
+        for child in node.children:
+            size = child.size / total_size
+            height = int(size * rect.height + 0.5)
+            if height > 1:
+                c_rect = Rect(rect.left, top, rect.width, height)
+                self.draw_rect(child.name, child.size, c_rect)
+                child.rect = c_rect
 
-            top += height
+                c2_rect = Rect(rect.left + rect.width, top, rect.width, height)
+                self.draw_children(child, c2_rect)
 
+                top += height
 
-def draw_rect(canvas: Canvas, name: str, size: int, rect: Rect) -> None:
-    canvas.create_rectangle(rect.left, rect.top, rect.width, rect.height)
+    def draw_rect(self, name: str, size: int, rect: Rect) -> None:
+        self.create_rectangle(rect.left, rect.top, rect.width, rect.height)
 
-    # TODO: Ability to disable show size
-    name = f"{name} ({size})"
-    canvas.create_text(rect.left, rect.top, text=name)
+        # TODO: Ability to disable show size
+        name = f"{name} ({size})"
+        self.create_text(rect.left, rect.top, text=name)
 
-
-def repaint(canvas: Canvas, node: Node, width: int, height: int) -> None:
-    rect = Rect(0, 0, int(width / n_cols), height)
-    node.clear_rects()
-    draw_node(canvas, node, rect)
+    def repaint(self, width: int, height: int) -> None:
+        rect = Rect(0, 0, int(width / n_cols), height)
+        self.node.clear_rects()
+        self.draw_node(rect)
 
 
 def main_loop(filename: str) -> None:
@@ -51,10 +56,10 @@ def main_loop(filename: str) -> None:
     tk.title("pyxdu")
     width = 800
     height = 600
-    canvas = Canvas(tk, width=width, height=height)
-    canvas.pack()
     top = parse_file(filename)
-    repaint(canvas, top, width, height)
+    canvas = XduCanvas(top, tk, width=width, height=height)
+    canvas.pack()
+    canvas.repaint(height, height)
 
     # Hack to bring the window to the foreground
     tk.attributes('-topmost', True)
